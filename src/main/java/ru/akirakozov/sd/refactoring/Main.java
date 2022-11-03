a457a1595688feca775530a51ea74f6ddd1c8683
Александр Щелочков <sky_line.5@rambler.ru>
2022-11-04 02:00:10 +0300

    Update Main: remove DataBase realisation into additional package.

--- src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -3,6 +3,8 @@ package ru.akirakozov.sd.refactoring;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import ru.akirakozov.sd.refactoring.databse.DataBase;
+import ru.akirakozov.sd.refactoring.databse.SQLDataBase;
 import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
 import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
@@ -17,27 +19,20 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) throws Exception {
         try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
+            DataBase db = new SQLDataBase(c);
 
-            stmt.executeUpdate(sql);
-            stmt.close();
+            Server server = new Server(8081);
+
+            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            context.setContextPath("/");
+            server.setHandler(context);
+
+            context.addServlet(new ServletHolder(new AddProductServlet(db)), "/add-product");
+            context.addServlet(new ServletHolder(new GetProductsServlet(db)),"/get-products");
+            context.addServlet(new ServletHolder(new QueryServlet(db)),"/query");
+
+            server.start();
+            server.join();
         }
-
-        Server server = new Server(8081);
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-
-        context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
-        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
-        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
-
-        server.start();
-        server.join();
     }
 }
